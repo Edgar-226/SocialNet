1b4eda3c2a3438009257c34222f3b2aa26527c1c
Edgar Bastida <edgar.bast.95@gmail.com>
2021-10-18 14:59:45 -0500

    mvc crud friends

--- back/controller/friendsController.js
+++ back/controller/friendsController.js
@@ -5,13 +5,37 @@ module.exports.friendsUserSelect = async (user) => {
     let result =await response.friendsUserSelect(user)
     return result;
 }
-module.exports.friendsInsert = async (user,friend) => {
+module.exports.friendsFindSelect = async (user, friend) => {
     let response = new friendsModel();
-    let result =await response.friendsInsert(user,friend)
+    let result =await response.friendsFindSelect(user, friend)
     return result;
 }
-module.exports.friendsDelete = async (user,friend) => {
+module.exports.friendsAdd = async (user,friend) => {
     let response = new friendsModel();
-    let result =await response.friendsDelete(user,friend)
+    let result =await response.friendsAdd(user,friend)
     return result;
 }
+
+module.exports.friendsRequest = async (user) => {
+    let response = new friendsModel();
+    let result =await response.friendsRequest(user)
+    return result;
+}
+module.exports.friendsRequest2 = async (user) => {
+    let response = new friendsModel();
+    let result =await response.friendsRequest2(user)
+    return result;
+}
+module.exports.friendsAccept = async (user,friend) => {
+    let response = new friendsModel();
+    let result =await response.friendsAccept(user,friend)
+    return result;
+}
+
+module.exports.friendsFriendSelect = async (user) => {
+    let response = new friendsModel();
+    let result =await response.friendsFriendSelect(user)
+    return result;
+}
+
+
--- back/views/friendsView.js
+++ back/views/friendsView.js
@@ -8,18 +8,41 @@ module.exports = async (app) => {
     app.get('/friends/selectUsers', autentication.userAutentication, async (req, res) => {
         const token = req.headers.authorization.split(' ')[1];
         let user = jwt.verify(token, process.env.SECRETKEY);
-        res.send(await friendsController.friendsUserSelect(user.data));
+        let friend = req.body
+        res.send(await friendsController.friendsUserSelect(user.data,friend));
     });
-    // app.post('/friends/insert', autentication.userAutentication, async (req, res) => {
-    //     const token = req.headers.authorization.split(' ')[1];
-    //     let user = jwt.verify(token, process.env.SECRETKEY);
-    //     let friend = req.body
-    //     res.send(await friendsController.friendsInsert(user.data, friend));
-    // });
-    // app.delete('/friends/delete', autentication.userAutentication, async(req, res) => {
-    //     const token = req.headers.authorization.split(' ')[1];
-    //     let user = jwt.verify(token, process.env.SECRETKEY);
-    //     let friend = req.body
-    //     res.send(await friendsController.friendsDelete(user.data, friend));
-    // });
+    app.get('/friends/findUsers', autentication.userAutentication, async (req, res) => {
+        const token = req.headers.authorization.split(' ')[1];
+        let user = jwt.verify(token, process.env.SECRETKEY);
+        let friend = req.body
+        res.send(await friendsController.friendsFindSelect(user.data,friend));
+    });
+    app.post('/friends/addfriend', autentication.userAutentication, async (req, res) => {
+        const token = req.headers.authorization.split(' ')[1];
+        let user = jwt.verify(token, process.env.SECRETKEY);
+        let friend = req.body
+        res.send(await friendsController.friendsAdd(user.data, friend));
+    });
+    app.get('/friends/getRequest', autentication.userAutentication, async (req, res) => {
+        const token = req.headers.authorization.split(' ')[1];
+        let user = jwt.verify(token, process.env.SECRETKEY);
+        res.send(await friendsController.friendsRequest(user.data));
+    });
+    app.get('/friends/getRequest2', autentication.userAutentication, async (req, res) => {
+        const token = req.headers.authorization.split(' ')[1];
+        let user = jwt.verify(token, process.env.SECRETKEY);
+        res.send(await friendsController.friendsRequest2(user.data));
+    });
+    app.post('/friends/acceptRequest', autentication.userAutentication, async (req, res) => {
+        const token = req.headers.authorization.split(' ')[1];
+        let user = jwt.verify(token, process.env.SECRETKEY);
+        let friend = req.body
+        res.send(await friendsController.friendsAccept(user.data, friend));
+    });
+    app.get('/friends/selectFriends', autentication.userAutentication, async (req, res) => {
+        const token = req.headers.authorization.split(' ')[1];
+        let user = jwt.verify(token, process.env.SECRETKEY);
+        
+        res.send(await friendsController.friendsFriendSelect(user.data));
+    });
 }
